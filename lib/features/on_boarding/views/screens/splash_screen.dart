9dc5cad48a6ae48098b9a12c1331a0b3160af028
Phalgun <phalgunmandava@gmail.com>
2026-03-10 11:37:31 +0530

    splash_screen changed

--- lib/features/on_boarding/views/screens/splash_screen.dart
+++ lib/features/on_boarding/views/screens/splash_screen.dart
@@ -1,31 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-import '../../../../core/navigation/app_router.dart';
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
+void main() {
+  runApp(const LeafyApp());
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class LeafyApp extends StatelessWidget {
+  const LeafyApp({super.key});
+
   @override
-  void initState() {
-    Future.delayed(const Duration(seconds: 3), () {
-      context.goNamed(AppRouter.loginName);
-    });
-    super.initState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
   }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFE9ECEB),
       body: Center(
-        child: Text(
-          'Splash Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 210,
+              width: 210,
+              decoration: BoxDecoration(
+                color: const Color(0xFFE6D6C9),
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Center(
+                child: Image.asset(
+                  "assets/images/Image.png",
+                  height: 220,
+                  width: 220,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 40),
+            const Text(
+              "Leafy",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF1B2B22),
+              ),
+            ),
+
+            const SizedBox(height: 6),
+            const Text(
+              "Bring nature home",
+              style: TextStyle(
+                fontSize: 13,
+                color: Colors.black54,
+              ),
+            ),
+
+            const SizedBox(height: 50),
+            SizedBox(
+              width: 200,
+              child: LinearProgressIndicator(
+                value: 0.35,
+                backgroundColor: Colors.grey,
+                valueColor:
+                    const AlwaysStoppedAnimation<Color>(Colors.green),
+                minHeight: 4,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            const Text(
+              "LOADING",
+              style: TextStyle(
+                letterSpacing: 2,
+                fontSize: 11,
+                color: Colors.black45,
+              ),
+            ),
+          ],
         ),
       ),
     );
