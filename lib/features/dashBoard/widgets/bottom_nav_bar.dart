e514d8c147727eaa407d396060944655259e4335
Ranadeep <kranadeep8@gmail.com>
2026-03-10 11:28:10 +0530

    bottom navigationbar

--- lib/features/dashBoard/widgets/bottom_nav_bar.dart
+++ lib/features/dashBoard/widgets/bottom_nav_bar.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class AppButtomNavBar extends StatelessWidget {
-  const AppButtomNavBar({super.key});
+class Figma extends StatefulWidget {
+  const Figma({super.key});
+
+  @override
+  State<Figma> createState() => _FigmaState();
+}
+
+class _FigmaState extends State<Figma> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    Center(child: Text("Shop Page")),
+    Center(child: Text("Explore Page")),
+    Center(child: Text("Cart Page")),
+    Center(child: Text("Account Page")),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.store), label: "SHOP"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "EXPLORE"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "CART",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "ACCOUNT"),
+        ],
+      ),
+    );
   }
 }
