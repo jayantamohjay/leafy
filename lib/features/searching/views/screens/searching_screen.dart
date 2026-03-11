190af45b048098cd7c60a91c9e687cd8769a855b
Ranadeep <kranadeep8@gmail.com>
2026-03-11 13:08:38 +0530

    changes

--- lib/features/searching/views/screens/searching_screen.dart
+++ lib/features/searching/views/screens/searching_screen.dart
@@ -12,47 +12,69 @@ class _SearchingScreenState extends State<SearchingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border(
             top: BorderSide(color: Colors.grey.shade300, width: 1),
           ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            /// Top Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'Good Morning',
-                  style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Good Morning',
+                      style: TextStyle(fontSize: 14, color: Colors.grey[700]),
+                    ),
+                    const SizedBox(height: 2),
+                    const Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 35, 97, 62),
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 2),
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 35, 97, 62),
+
+                /// Profile Icon
+                Container(
+                  width: 36,
+                  height: 36,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color(0xFF2A3A31),
+                      width: 1,
+                    ),
+                  ),
+                  child: const Icon(
+                    Icons.person,
+                    color: Color.fromARGB(255, 0, 85, 37),
+                    size: 20,
                   ),
                 ),
               ],
             ),
 
-            Container(
-              width: 36,
-              height: 36,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Color(0xFF2A3A31), width: 1),
-              ),
-              child: Icon(
-                Icons.person,
-                color: Color.fromARGB(255, 0, 85, 37),
-                size: 20,
+            const SizedBox(height: 19),
+
+            /// Search Bar
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Search for your next plant.....',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
               ),
             ),
           ],
