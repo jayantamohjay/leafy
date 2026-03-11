62d7a902efa6c6cc4270cf198eab124a9c004d61
Jayanta Mahato <jayanta.mohjay@gmail.com>
2026-03-11 12:21:14 +0530

    Remove duplicate debugShowCheckedModeBanner property from MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: AppTheme().lightTheme(),
       routerConfig: AppRouter.router,
     );
