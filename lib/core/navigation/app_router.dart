475afbf0410235c4ab0847077530fde97fc63cb7
Phalgun <phalgunmandava@gmail.com>
2026-03-11 12:04:26 +0530

    changes

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -46,7 +46,7 @@ class AppRouter {
       GoRoute(
         path: '/$searchName',
         name: searchName,
-        builder: (context, state) => const SearchingScreen(),
+        builder: (context, state) => const DiscoverPage(),
       ),
       GoRoute(
         path: '/$profileName',
--- lib/features/searching/views/screens/searching_screen.dart
+++ lib/features/searching/views/screens/searching_screen.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 
-class SearchingScreen extends StatelessWidget {
-  const SearchingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Searching')),
-      body: const Center(child: Text('Searching Content')),
-    );
-  }
-}
-
 class DiscoverPage extends StatelessWidget {
   const DiscoverPage({super.key});
 
